52960caa14252b70ad128de4d6c77063f985b777
Your Name <you@example.com>
2019-05-01 10:51:30 -0500

    fix

--- src/autoload.php
+++ src/autoload.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__.'/../vendor/autoload.php';
+//require_once __DIR__.'/../vendor/autoload.php';
 
 //--- Cargar los files dentro de Resources
 $files  = scandir(__DIR__.'/Resource');
