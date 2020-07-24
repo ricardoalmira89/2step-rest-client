40ef5b17a1c8ba500dce009d2bce6138269b0c48
azcro <develored3@gmail.com>
2020-07-24 11:26:48 -0500

    cleaning

--- README.md
+++ README.md
@@ -9,15 +9,17 @@ Instalation
 ===========================
 composer require "TwoStepReviews/rest-client @dev"
 
-Uso
+Usage
 ===========================
 <?php
 
-use TwoStepReviews\Client;
 require_once 'vendor/autoload.php';
 
+use TwoStepReviews\Client;
+use TwoStepReviews\Type\RoleType;
+
 $parameters = array(
-    'api' => "http://api.2stepreviews.com.com",
+    'api' => "http://api.2stepreviews.com",
     'client_id' => "your client id",
     'client_secret' => "your client secret",
     'username' => "your username",
@@ -28,3 +30,31 @@ $client = new Client($parameters);
 
 // Get a review list
 $response = $client->get('review')->index();
+
+// Updates the maxLocations property to a given company (company_id = 56, max_locations = 2)
+$response = $client->get('company')->updateMaxLocations(56, 2);
+
+// Creates a enterprise user ----
+$response = $client->get('user')->create(array(
+    "email" => "enterprise@somecompany.com",
+    "nombre" =>  "Mr. Enterprise",
+    "password" => "TZ886n6c<)-",
+    "phone" => "+122558798",
+    "role_id" => RoleType::ROLE_ENTERPRISE
+));
+
+// Signs up a new client ----
+$response = $client->get('signup')->signup(array(
+    "nombre" => "Mr. Enterprise",
+    "email" => "enterprise@somecompany.com",
+    "phone" => "789896598",
+    "password" => "TZ886n6c<)-",
+    "company_name" => "H&R Films",
+    "twilio_number" => "31458794",
+    "area_code" => "347",
+    "acc_type" => 2,
+    "max_locations" => 2
+));
+
+// Asociates the enterprise user_id=547 with the company_id=54
+$res = $client->get('user')->associateAccount(547, 54);
--- index.php
+++ index.php
@@ -1,60 +1,8 @@
 <?php
-/**
- * Created by PhpStorm.
- * User=> root
- * Date=> 30/11/18
- * Time=> 08=>12 AM
- */
 
 require 'vendor/autoload.php';
 
 use TwoStepReviews\Client;
 use TwoStepReviews\Type\RoleType;
 
-try{
-
-    $client = new Client(array(
-        "api"  =>  "http://localhost/reviews/api/web/app_dev.php",
-        "client_id"=>"2_4bcbxd9e24g0gk4swg0kwgcwg4o8k8g4g888kwc44gcc0gwwk4",
-        "client_secret"=>"4ok2x70rlfokc8g0wws8c8kwcokw80k44sg48goc0ok4w0so0k",
-        "username"=>"heidy@resteasymarketing.com",
-        "password"=>"wailea911",
-        "session_type" => "local"
-    ));
-
-    //--- Se company with id=56 to maxlocations=2 ---
-//    $res = $client->get('company')->updateMaxLocations(56, 2);
-
-    //--- Create a enterprise user ----
-//    $res = $client->get('user')->create(array(
-//        "email" => "enterprise@somecompany.com",
-//        "nombre" =>  "Mr. Enterprise",
-//        "password" => "TZ886n6c<)-",
-//        "phone" => "+122558798",
-//        "role_id" => RoleType::ROLE_ENTERPRISE
-//    ));
-
-    //--- Signup a new client ----
-//    $res = $client->get('signup')->signup(array(
-//        "nombre" => "Mr. Enterprise",
-//        "email" => "enterprise@somecompany.com",
-//        "phone" => "789896598",
-//        "password" => "TZ886n6c<)-",
-//        "company_name" => "H&R Films",
-//        "twilio_number" => "31458794",
-//        "area_code" => "347",
-//        "acc_type" => 2,
-//        "max_locations" => 2
-//    ));
-
-    //--- Asociates the enterprise user_id=547 with the company_id=54
-    $res = $client->get('user')->associateAccount(547, 54);
-
-    dump($res);die();
-
-} catch (Exception $ex){
-
-    dump($ex);
-    die();
-
-}
+//-- This file will be used only for testing purposes....
